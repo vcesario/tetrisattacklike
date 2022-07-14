a3baa1cf4e4d8b947493c899cbbfa16229256390
victor cesario <victor.cesario@hotmail.com>
2022-07-14 20:59:40 -0300

    iniciando refatoracao para grid logico

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,42 +13,16 @@ public class GameManager : MonoBehaviour
     [Space]
     public GameObject examples;
 
-    private GridBall[,] grid;
+    private GridCell[,] grid;
     private Vector2Int selector_gridPosition;
     private int selectorOrientation; // 0 = horizontal, 1 = vertical
     private bool lockInput;
 
-    IEnumerator Start()
+    private void Start()
     {
         examples.SetActive(false);
 
-        lockInput = true;
-        selector.gameObject.SetActive(false);
-
-        grid = new GridBall[(int)gridSize.x, (int)gridSize.y];
-
-        for (int j = 0; j < gridSize.y; j++)
-        {
-            for (int i = 0; i < gridSize.x; i++)
-            {
-                // calcular posX de forma centralizada, caso seja par ou impar
-                float lerpBounds = 0;
-                if (gridSize.x % 2 == 0) // se par
-                    lerpBounds = gridSize.z / 2f + ((gridSize.x / 2f) - 1) * gridSize.z;
-                else // se impar
-                    lerpBounds = Mathf.Floor(gridSize.x / 2f) * gridSize.z;
-                float posX = Mathf.Lerp(-lerpBounds, lerpBounds, i / (gridSize.x - 1));
-
-                spawnBall(posX, i, j);
-
-                yield return new WaitForSeconds(.1f);
-            }
-        }
-
-        selector.gameObject.SetActive(true);
-        lockInput = false;
-        updateSelectorWorldPosition();
-        updateSelectorWorldRotation();
+        resetGrid();
     }
 
     void Update()
@@ -68,7 +42,7 @@ public class GameManager : MonoBehaviour
         int materialIndex = Random.Range(0, ballMaterials.Count);
         newRenderer.sharedMaterial = ballMaterials[materialIndex];
 
-        grid[gridI, gridJ] = newBall;
+        grid[gridI, gridJ] = new GridCell(newBall, materialIndex);
     }
 
     private void inputs()
@@ -102,6 +76,10 @@ public class GameManager : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.Return))
         {
             swapBalls();
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            resetGrid();
         }
     }
 
@@ -192,28 +170,27 @@ public class GameManager : MonoBehaviour
 
     private void updateSelectorWorldPosition()
     {
-        GridBall selectedBall = grid[selector_gridPosition.x, selector_gridPosition.y];
-        Vector3 selectedBall_worldPosition = selectedBall.transform.position;
+        GridCell selectedCell = grid[selector_gridPosition.x, selector_gridPosition.y];
+        Vector3 selectedBall_worldPosition = selectedCell.ball.transform.position;
 
         // para casos horizontais, a posicao do seletor sera a media entre a bola selecionada e a bola a direita
         if (selectorOrientation == 0)
         {
-            GridBall rightBall = grid[selector_gridPosition.x + 1, selector_gridPosition.y];
-            Vector3 rightBall_worldPosition = rightBall.transform.position;
+            GridCell rightCell = grid[selector_gridPosition.x + 1, selector_gridPosition.y];
+            Vector3 rightBall_worldPosition = rightCell.ball.transform.position;
 
             selector.transform.position = (selectedBall_worldPosition + rightBall_worldPosition) / 2;
         }
         else // para casos verticais, e a media entre a selecionada e a de cima
         {
-            GridBall topBall = grid[selector_gridPosition.x, selector_gridPosition.y + 1];
-            Vector3 topBall_worldPosition = topBall.transform.position;
+            GridCell topCell = grid[selector_gridPosition.x, selector_gridPosition.y + 1];
+            Vector3 topBall_worldPosition = topCell.ball.transform.position;
 
             selector.transform.position = (selectedBall_worldPosition + topBall_worldPosition) / 2;
         }
     }
 
     // trocar bolas de posicao
-
     private void swapBalls()
     {
         StartCoroutine(_swapBalls());
@@ -224,29 +201,29 @@ public class GameManager : MonoBehaviour
         lockInput = true;
 
         // travar todas as fisicas para nao acontecer acidentes
-        foreach (var ball in grid)
-            ball.thisRigidbody.isKinematic = true;
+        foreach (var cell in grid)
+            cell.ball.thisRigidbody.isKinematic = true;
 
         // determino quais sao as duas bolas a serem trocadas
-        GridBall ballA = grid[selector_gridPosition.x, selector_gridPosition.y];
-        GridBall ballB;
+        GridCell cellA = grid[selector_gridPosition.x, selector_gridPosition.y];
+        GridCell cellB;
 
         Vector2Int ballB_gridPosition;
         if (selectorOrientation == 0)
-            ballB_gridPosition = new Vector2Int(selector_gridPosition.x +1 , selector_gridPosition.y);
+            ballB_gridPosition = new Vector2Int(selector_gridPosition.x + 1, selector_gridPosition.y);
         else
             ballB_gridPosition = new Vector2Int(selector_gridPosition.x, selector_gridPosition.y + 1);
-        ballB = grid[ballB_gridPosition.x, ballB_gridPosition.y];
+        cellB = grid[ballB_gridPosition.x, ballB_gridPosition.y];
 
         // calculo para qual posicao elas vao
-        Vector3 aOrigin = ballA.transform.position;
-        Vector3 bOrigin = ballB.transform.position;
-        Vector3 aDest = ballB.transform.position;
-        Vector3 bDest = ballA.transform.position;
+        Vector3 aOrigin = cellA.ball.transform.position;
+        Vector3 bOrigin = cellB.ball.transform.position;
+        Vector3 aDest = cellB.ball.transform.position;
+        Vector3 bDest = cellA.ball.transform.position;
 
         // realizo a troca logica
-        grid[selector_gridPosition.x, selector_gridPosition.y] = ballB;
-        grid[ballB_gridPosition.x, ballB_gridPosition.y] = ballA;
+        grid[selector_gridPosition.x, selector_gridPosition.y] = cellB;
+        grid[ballB_gridPosition.x, ballB_gridPosition.y] = cellA;
 
         // realizo a troca de posicao
         float swapDuration = .18f;
@@ -254,17 +231,75 @@ public class GameManager : MonoBehaviour
         while (swapElapsed < swapDuration)
         {
             swapElapsed += Time.deltaTime;
-            ballA.transform.position = Vector3.Lerp(aOrigin, aDest, swapElapsed / swapDuration);
-            ballB.transform.position = Vector3.Lerp(bOrigin, bDest, swapElapsed / swapDuration);
+            cellA.ball.transform.position = Vector3.Lerp(aOrigin, aDest, swapElapsed / swapDuration);
+            cellB.ball.transform.position = Vector3.Lerp(bOrigin, bDest, swapElapsed / swapDuration);
             yield return 0;
         }
-        ballA.transform.position = aDest;
-        ballB.transform.position = bDest;
+        cellA.ball.transform.position = aDest;
+        cellB.ball.transform.position = bDest;
 
         // destravar fisicas
-        foreach (var ball in grid)
-            ball.thisRigidbody.isKinematic = false;
+        foreach (var cell in grid)
+            cell.ball.thisRigidbody.isKinematic = false;
 
         lockInput = false;
+    }
+
+    private void resetGrid()
+    {
+        // deletar grid anterior, se houver
+        if (grid != null)
+        {
+            for (int j = 0; j < gridSize.y; j++)
+            {
+                for (int i = 0; i < gridSize.x; i++)
+                {
+                    if (grid[i, j] != null)
+                    {
+                        Destroy(grid[i, j].ball.gameObject);
+                        grid[i, j] = null;
+                    }
+                }
+            }
+        }
+
+        StartCoroutine(_spawnNewGrid());
+    }
+
+    private IEnumerator _spawnNewGrid()
+    {
+        lockInput = true;
+        selector.gameObject.SetActive(false);
+
+        initializeGrid();
+
+
+        for (int j = 0; j < gridSize.y; j++)
+        {
+            for (int i = 0; i < gridSize.x; i++)
+            {
+                // calcular posX de forma centralizada, caso seja par ou impar
+                float lerpBounds = 0;
+                if (gridSize.x % 2 == 0) // se par
+                    lerpBounds = gridSize.z / 2f + ((gridSize.x / 2f) - 1) * gridSize.z;
+                else // se impar
+                    lerpBounds = Mathf.Floor(gridSize.x / 2f) * gridSize.z;
+                float posX = Mathf.Lerp(-lerpBounds, lerpBounds, i / (gridSize.x - 1));
+
+                spawnBall(posX, i, j);
+
+                yield return new WaitForSeconds(.1f);
+            }
+        }
+
+        selector.gameObject.SetActive(true);
+        lockInput = false;
+        updateSelectorWorldPosition();
+        updateSelectorWorldRotation();
+    }
+
+    private void initializeGrid()
+    {
+        grid = new GridCell[(int)gridSize.x, (int)gridSize.y];
     }
 }
--- Assets/Scripts/GridBall.cs
+++ Assets/Scripts/GridBall.cs
@@ -6,3 +6,16 @@ public class GridBall : MonoBehaviour
 {
     public Rigidbody thisRigidbody;
 }
+
+public class GridCell
+{
+    public GridBall ball;
+    public int ballType;
+
+    public GridCell(GridBall _ball, int _type)
+    {
+        ball = _ball;
+        ballType = _type;
+    }
+}
+
